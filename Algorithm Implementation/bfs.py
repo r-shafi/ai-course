350ec748bfa9fb5fec242281c29ecf8d20f862d1
Shafi Rayhan <rayhanshafi7@gmail.com>
2025-07-07 21:49:45 +0600

    Implement Breadth-First Search algorithm for pathfinding

---
+++ Algorithm Implementation/bfs.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+
+def bfs(graph, start, goal):
+    visited = set()
+    queue = deque([(start, [start])])
+    while queue:
+        node, path = queue.popleft()
+        if node == goal:
+            return path
+        if node not in visited:
+            visited.add(node)
+            for neighbor in graph[node]:
+                queue.append((neighbor, path + [neighbor]))
+    return None
+
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+print(bfs(graph, 'A', 'F'))
