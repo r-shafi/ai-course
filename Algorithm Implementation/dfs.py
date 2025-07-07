57b9c58223e9e317cd2fe558bc98abff190c7a99
Shafi Rayhan <rayhanshafi7@gmail.com>
2025-07-07 21:49:59 +0600

    Implement Depth-First Search algorithm for pathfinding

---
+++ Algorithm Implementation/dfs.py
@@ -0,0 +1,25 @@
+def dfs(graph, start, goal, visited=None, path=None):
+    if visited is None:
+        visited = set()
+    if path is None:
+        path = [start]
+    if start == goal:
+        return path
+    visited.add(start)
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            result = dfs(graph, neighbor, goal, visited, path + [neighbor])
+            if result:
+                return result
+    return None
+
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+print(dfs(graph, 'A', 'F'))
