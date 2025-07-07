2fa185cc51841b74abf003eae82da183ca70058e
Shafi Rayhan <rayhanshafi7@gmail.com>
2025-07-07 21:50:04 +0600

    Implement Depth-Limited Search algorithm for pathfinding

---
+++ Algorithm Implementation/depth_limited_search.py
@@ -0,0 +1,28 @@
+def depth_limited(graph, start, goal, limit, visited=None, path=None):
+    if visited is None:
+        visited = set()
+    if path is None:
+        path = [start]
+    if start == goal:
+        return path
+    if limit <= 0:
+        return None
+    visited.add(start)
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            result = depth_limited(graph, neighbor, goal, limit -
+                                   1, visited, path + [neighbor])
+            if result:
+                return result
+    return None
+
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+print(depth_limited(graph, 'A', 'F', 3))
